19811fd7f3d9eba7a9ac73c949f9c1c4c50a6027
nguyencaoantuong <ntuong@proton.me>
2025-12-02 19:45:19 +0700

    fix: unresolved reference (p2)

--- app/src/main/java/com/eclipseLaboratory/eclipseLabs/ui/theme/Theme.kt
+++ app/src/main/java/com/eclipseLaboratory/eclipseLabs/ui/theme/Theme.kt
@@ -3,6 +3,7 @@ package com.eclipseLaboratory.eclipseLabs.ui.theme
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
